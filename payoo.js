09fef08b3533a7fcdc825ba0c15cff66dac98a81
monzila-akter <monzila01727@gmail.com>
2024-09-19 21:14:34 +0600

    added link up to loggin button

--- payoo.js
+++ payoo.js
@@ -3,9 +3,16 @@
 document.getElementById('btn-login').addEventListener('click', function(event){
     // step-2--preventDefault for not reload browser
     event.preventDefault();
-    console.log('login button clicked')
     // step-3 get the phone number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+    const pinNumber = document.getElementById('pin-number').value;
+    // step-4 add condition to go another page but this is wrong way
+    if(phoneNumber === '5' && pinNumber === '1234'){
+       window.location.href = 'home.html';
+        // console.log('you are logged in');
+    }
+    else{
+        alert('wrong phone number or pin number');
+    }
 })
 
